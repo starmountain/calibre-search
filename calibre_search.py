54c221001dc473fe832503f4e537d3f720b7b1f7
starmountain <starmountain2014@outlook.com>
2017-06-01 19:57:50 +0800

    Update calibre_search.py

--- calibre_search.py
+++ calibre_search.py
@@ -1,7 +1,8 @@
 import _sqlite3
 import os
 
-def calibre_search(library_path,database_path,book_name):
+def calibre_search(library_path,book_name):
+    database_path=library_path+'metadata.db'
     conn=_sqlite3.connect(database_path)
     c=conn.cursor()
     query="select path from books where title like \'%"+book_name+"%\';"
@@ -12,7 +13,8 @@ def calibre_search(library_path,database_path,book_name):
     realpath = realpath.replace(']', '\]')
     realpath = realpath.replace('(', '\(')
     realpath = realpath.replace(')', '\)')
-    print(realpath)
+    #print(realpath)
     realpath="open "+library_path+realpath
-    print(realpath)
+    #print(realpath)
     os.system(realpath)
+
